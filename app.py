326228fcdcb85ba6214bd6da7b1dc2e9b2e9b985
samukapsilva <samukapsilva@gmail.com>
2025-04-30 13:18:17 +0100

    add desert to the menu

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
@@ -7,10 +10,20 @@ restaurante_praca.reeber_avaliacao('Joao', 8)
 restaurante_praca.reeber_avaliacao('Maria', 10)
 restaurante_praca.reeber_avaliacao('Emy', 5)
 
+bebida_suco = Bebida ('Suco de Melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+sobremesa_pudim = Sobremesa('Pudim', 7.5,'','grande', 'Pudim de leite condensado')
+sobremesa_pudim.aplicar_desconto()
 
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(sobremesa_pudim)
 
 def main():
-    Restaurante.listar_restaurantes()
+    #Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 
 if __name__ == '__main__':
